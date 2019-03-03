be112d2dfe2ccb5effb749a0239f1a7d1c238490
Stellakim1230 <stellakim1230@gmail.com>
2019-03-03 18:08:42 +0900

    add react get start example.

---
+++ src/components/LikeButton.js
@@ -0,0 +1,20 @@
+import React, { Component, Fragment } from 'react';
+
+class LikeButton extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { liked: false }
+  }
+  
+  render() {
+    if (this.state.liked) return 'You liked this.'
+    
+    return (
+      <Fragment>
+        <button onClick={() => this.setState({ liked: true })}>Like</button>
+      </Fragment>
+    );
+  }
+}
+
+export default LikeButton;
